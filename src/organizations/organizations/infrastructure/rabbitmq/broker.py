858c3ffd8058660151241266296ab2db599136e5
kyotoqqe <97350762+kyotoqqe@users.noreply.github.com>
2025-11-21 00:31:23 +0200

    Refactor(Organizations): Fix conflict

--- src/organizations/organizations/infrastructure/rabbitmq/broker.py
+++ src/organizations/organizations/infrastructure/rabbitmq/broker.py
@@ -1,21 +1,14 @@
 from src.core.rabbitmq.message_broker import RabbitMQMessageBroker
 
-<<<<<<< Updated upstream
-from src.organizations import EVENT_HANDLERS_FOR_INJECTION, COMMAND_HANDLERS_FOR_INJECTION
-=======
+
 from src.organizations import DOMAIN_EVENT_HANDLERS_FOR_INJECTION, EXTERNAL_EVENT_HANDLERS_FOR_INJECTION, \
     COMMAND_HANDLERS_FOR_INJECTION
->>>>>>> Stashed changes
 
 #create interface
 class OrganizationRabbitMQBroker(RabbitMQMessageBroker):
     queue = "organization"
     exchange = "organization"
-<<<<<<< Updated upstream
-    event_handlers = EVENT_HANDLERS_FOR_INJECTION
-=======
     event_handlers = DOMAIN_EVENT_HANDLERS_FOR_INJECTION | EXTERNAL_EVENT_HANDLERS_FOR_INJECTION
->>>>>>> Stashed changes
     command_handlers = COMMAND_HANDLERS_FOR_INJECTION
     router_keys_for_queues = {
         "organization":[
